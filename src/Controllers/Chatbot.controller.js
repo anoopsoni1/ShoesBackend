7ad739f5711b15892938546bbf2d3284c1cdeb1b
anoopsoni1 <sonianup8965@gmail.com>
2025-09-03 00:34:09 +0530

    hello

--- src/Controllers/Chatbot.controller.js
+++ src/Controllers/Chatbot.controller.js
@@ -2,19 +2,29 @@ import dotenv from "dotenv";
 import { GoogleGenAI } from "@google/genai";
 import { Asynchandler } from "../utils/Asynchandler.js";
 import {ApiResponse} from "../utils/Apiresponse.js"
+import {ApiError} from "../utils/Apierror.js"
 
 dotenv.config();
 
 const client = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
 
 const Chatbot = Asynchandler(async (req, res) => {
-  const { question, products } = req.body;
+  const { question, products, user} = req.body;
+
   try {
-    const prompt = `
-You are a helpful AI for a shoe website.
+    const prompt =  `
+You are a helpful AI for a shoe website. 
+Here is the logged-in user: ${JSON.stringify(user)}
 Here is the product catalog: ${JSON.stringify(products)}
 User asked: "${question}"
-Provide recommendations based only on this catalog.
+
+Rules:
+- If the question is about products → answer only using the catalog.
+- If the question is about delivery → answer using the orders data (4day).
+- If the user asks for order status → return the status from the orders data.
+- If asked for their name → use the user object.
+- If the question is outside this scope → politely decline.
+- if the question is about the owner of this website ->  return the name Anoop soni
     `
     const result = await client.models.generateContent({
       model: "gemini-1.5-flash",
